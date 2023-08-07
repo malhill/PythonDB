f3e85d2846cb5c708fada3fbcc0e7f911f8add93
malhill <malcolmhc@msn.com>
2023-08-07 14:15:03 -0400

    read and update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tabulate import tabulate
 # https://api.mongodb.com/python/current
 # Connecting to database
 client = pymongo.MongoClient("mongodb://localhost:27017/")
+print(client)
 filter={  }
 
 
@@ -21,17 +22,32 @@ def read_db(collection):
     try:
         documents = collection.find()
         for doc in documents:
+            titles = [ "Name", "Age"]
             name = doc['name']
             age = doc['age'] 
+            tables = tabulate(documents, showindex='never')
             # print(f'Name:{name} Age:{age}')
-            print(tabulate(documents))
+            print(tables)
     except pymongo.errors.PyMongoError as e:
         print("Error reading documents:", e)
 # print(read_db(collection))
 
+# Updating Database!
+def update_db(collection, query, update):
+    try:
+        result = collection.update_one(query, {"$set": update})
+        if result.modified_count > 0:
+            print("Db pdated!")
+        else:
+            print("Nothing found.")
+    except pymongo.errors.PyMongoError as e:
+        print("Error updating document: ", e)
+# print(update_db)
+
+
 # Creating a terminal menu
 def print_menu():
-    print("\n======== MongoDB CRUD Operations ========")
+    print("\n======== MongoDB CRUD ========")
     print("1. Create a document")
     print("2. Read all documents")
     print("3. Update a document")
@@ -47,6 +63,13 @@ else:
   print_menu()  
   action 
      
+if action == "3":
+    name = input("Enter name in data: ")
+    update_age = int(input("Enter the new age: "))
+    update_name = {"name": name }
+    update_age = {"age": update_age}
+    update_db(collection, update_name, update_age)
+
 
 
 # Inserting multiple documents manually
@@ -58,10 +81,8 @@ else:
 # print(db)
 
 # The code below is a sqlite3 db with no sql file needed
-
 # Connect to the database (it will create a new one if it doesn't exist)
 # connection = sqlite3.connect('example.db')
-
 # Create a cursor object to execute SQL commands
 # cursor = connection.cursor()
 
@@ -72,7 +93,6 @@ else:
 # Insert some data into the table
 # cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('John Doe', 30))
 # cursor.execute("INSERT INTO users (name, age) VALUES (?, ?)", ('Jane Smith', 25))
-
 # for row in cursor.execute("select * from users"):
     # print(row)
 
